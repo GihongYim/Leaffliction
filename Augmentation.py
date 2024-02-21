450fd5947e4a66a2d65f83b66c03b419420ae315
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-21 18:32:30 +0900

    augmentation.py

--- Augmentation.py
+++ Augmentation.py
@@ -9,18 +9,34 @@ def augmentation(path: str):
         original_img, path, filename = pcv.readimage(path)
     except Exception as e:
         print(f"{e.__class__.__name__}: {e}")
-    os.path.join("augmented_directory", path)
-    print(filename)
-    split_filename = path.split('.')
-    path[0] = path[0] + "_Flip"
+    flip(original_img, path, filename, direction='vertical')
+    rotate(original_img, path, filename, rotation_deg=45, crop=True)
+
+
+def flip(img: np.ndarray, path, filename, direction='vertical'):
+    augmentation_dir = os.path.join("augmented_directory", path)
+    if not os.path.exists(augmentation_dir):
+        os.makedirs(augmentation_dir)
+    split_filename = filename.split('.')
+    split_filename[0] = split_filename[0] + "_Flip"
     flip_filename = '.'.join(split_filename)
-    print(flip_filename)
-    flip(original_img)
+    flipped = pcv.flip(img=img, direction=direction)
+    pcv.print_image(flipped, filename=os.path.join(augmentation_dir,
+                                                   flip_filename))
 
 
-def flip(img: np.ndarray):
-    flipped = pcv.flip(img=img, direction='horizontal')
-    pcv.print_image(flipped, path)
+def rotate(img: np.ndarray, path, filename, rotation_deg, crop=True):
+    augmentation_dir = os.path.join("augmented_directory", path)
+    if not os.path.exists(augmentation_dir):
+        os.makedirs(augmentation_dir)
+    split_filename = filename.split('.')
+    split_filename[0] = split_filename[0] + "_Rotate"
+    flip_filename = '.'.join(split_filename)
+    rotated = pcv.transform.rotate(img=img,
+                                   rotation_deg=rotation_deg,
+                                   crop=crop)
+    pcv.print_image(rotated, filename=os.path.join(augmentation_dir,
+                                                   flip_filename))
 
 
 if __name__ == "__main__":

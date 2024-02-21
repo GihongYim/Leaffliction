51acce32cbfa952da55572028bb7fca97cb1719d
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-21 19:24:41 +0900

    shear

--- Augmentation.py
+++ Augmentation.py
@@ -10,13 +10,22 @@ def augmentation(path: str):
         original_img, path, filename = pcv.readimage(path)
     except Exception as e:
         print(f"{e.__class__.__name__}: {e}")
+    # flip
     flip(original_img, path, filename, direction='vertical')
+
+    # rotate
     rotate(original_img, path, filename, rotation_deg=45, crop=True)
+
+    # skew
     skew_factor = 0.5
     skew_matrix = np.array([[1, skew_factor, 0], [0, 1, 0]])
     skew(original_img, path, filename,
-         skew_matrix=skew_matrix,
-         skew_factor=skew_factor)
+         skew_matrix=skew_matrix)
+    # shear
+    shear_factor = 0.5
+    shear_matrix = np.array([[1, shear_factor, 0], [0, 1, 0]])
+    shear(original_img, path, filename,
+          shear_matrix=shear_matrix)
 
 
 def flip(img: np.ndarray, path, filename, direction='vertical'):
@@ -37,40 +46,40 @@ def rotate(img: np.ndarray, path, filename, rotation_deg, crop=True):
         os.makedirs(augmentation_dir)
     split_filename = filename.split('.')
     split_filename[0] = split_filename[0] + "_Rotate"
-    flip_filename = '.'.join(split_filename)
+    rotate_filename = '.'.join(split_filename)
     rotated = pcv.transform.rotate(img=img,
                                    rotation_deg=rotation_deg,
                                    crop=crop)
     pcv.print_image(rotated, filename=os.path.join(augmentation_dir,
-                                                   flip_filename))
+                                                   rotate_filename))
 
 
-def rotate(img: np.ndarray, path, filename, rotation_deg, crop=True):
-    augmentation_dir = os.path.join("augmented_directory", path)
-    if not os.path.exists(augmentation_dir):
-        os.makedirs(augmentation_dir)
-    split_filename = filename.split('.')
-    split_filename[0] = split_filename[0] + "_Rotate"
-    flip_filename = '.'.join(split_filename)
-    rotated = pcv.transform.rotate(img=img,
-                                   rotation_deg=rotation_deg,
-                                   crop=crop)
-    pcv.print_image(rotated, filename=os.path.join(augmentation_dir,
-                                                   flip_filename))
-
-
-def skew(img: np.ndarray, path, filename, skew_matrix, skew_factor):
+def skew(img: np.ndarray, path, filename, skew_matrix):
     augmentation_dir = os.path.join("augmented_directory", path)
     if not os.path.exists(augmentation_dir):
         os.makedirs(augmentation_dir)
     split_filename = filename.split('.')
     split_filename[0] = split_filename[0] + "_Skew"
-    flip_filename = '.'.join(split_filename)
+    skew_filename = '.'.join(split_filename)
 
     rows, cols, _ = img.shape
     skewed_img_array = cv2.warpAffine(img, skew_matrix, (cols, rows))
     pcv.print_image(skewed_img_array, filename=os.path.join(augmentation_dir,
-                                                            flip_filename))
+                                                            skew_filename))
+
+
+def shear(img: np.ndarray, path, filename, shear_matrix):
+    augmentation_dir = os.path.join("augmented_directory", path)
+    if not os.path.exists(augmentation_dir):
+        os.makedirs(augmentation_dir)
+    split_filename = filename.split('.')
+    split_filename[0] = split_filename[0] + "_Shear"
+    shear_filename = '.'.join(split_filename)
+
+    rows, cols, _ = img.shape
+    sheared_img_array = cv2.warpAffine(img, shear_matrix, (cols, rows))
+    pcv.print_image(sheared_img_array, filename=os.path.join(augmentation_dir,
+                                                             shear_filename))
 
 
 if __name__ == "__main__":

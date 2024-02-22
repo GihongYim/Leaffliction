ff1cacb752c0a0862062b9456c4facf2c7798ff5
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-22 19:59:47 +0900

    asdf

--- Transformation.py
+++ Transformation.py
@@ -2,23 +2,33 @@ import os
 import sys
 import argparse
 from plantcv import plantcv as pcv
+import matplotlib.pyplot as plt
 
 
-def transformation(path: str):
-    original_img, path, filename = pcv.readimage(path)
+def transformation(filename: str):
+    print('what')
+    try:
+        img, path, filename = pcv.readimage(filename)
+    except Exception as e:
+        print(f"{e.__class__.__name}: {e}")
+        exit(1)
+    gaussian_blur_img = pcv.gaussian_blur(img, ksize=(11, 11))
+    plt.imshow(gaussian_blur_img)
+    plt.show()
+    mask_img = pcv.apply_mask(img, mask= ,mask_color='green')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('-src', type=str)
-    parser.add_argument('-dst', type=str)
+    parser.add_argument('filename', default=None)
+    parser.add_argument('-src', type=str, default=None)
+    parser.add_argument('-dst', type=str, default=None)
     parser.add_argument('-mask', action='store_true')
     args = parser.parse_args()
 
     src_dir = args.src
     dst_dir = args.dst
     mask = args.mask
-    print(src_dir)
-    print(dst_dir)
-    print(mask)
+    if args.filename is not None:
+        transformation(args.filename)
+
